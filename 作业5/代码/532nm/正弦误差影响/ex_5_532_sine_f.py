3a0cf7fa365ac15de8b0a3134180a85e77df2030
Huang Runhua <45262463+HuangRunHua@users.noreply.github.com>
2021-12-16 14:28:06 +0800

    添加正弦噪声频率对光谱曲线的影响

---
+++ 作业5/代码/532nm/正弦误差影响/ex_5_532_sine_f.py
@@ -0,0 +1,109 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from scipy.fftpack import fft
+from pylab import *
+import matplotlib.gridspec as gridspec
+
+##########################################################################
+# 本程序模拟动镜倾斜误差影响下的傅里叶变换光谱测量系统的光谱测量曲线
+# 程序具体参数如下：
+#   - 采样间隔选取 79.1nm
+#   - 干涉图波长选取 532nm
+#   - 干涉图采样点数选取 2^12（该点数下仿真图像最佳）
+#   - 本实验只叠加一种噪声---正弦噪声
+#   - 实验目的在于模拟不同频率的正弦噪声对信号的影响
+#   - 如何使用本代码：修改正弦噪声的频率即可
+##########################################################################
+
+def GetFFT(I0, Iw0, n0):
+    Y0 = 2*abs(fft(I0,n0))
+    Y0_max = max(Y0);
+    Y0 = Y0/Y0_max
+    Y0 = Y0[:int(n0/2)]
+
+    Yw0 = 2*abs(fft(Iw0,n0))
+    Yw0_max = max(Yw0);
+    Yw0 = Yw0/Yw0_max
+    Yw0 = Yw0[:int(n0/2)]
+    return Y0, Yw0
+
+# 波长为532nm
+laimda0 = 532*10**(-9)
+# 79.1nm的采样间隔 
+i = 79.1*10**(-9)
+# 中心点的采样频
+sigma0 = 1/laimda0
+p1 = (-1)*(2**11)*79.1*10**(-9)
+# 2**n个点 
+p2 = (2**11-1)*79.1*10**(-9)
+# 无补零 
+x0 = np.arange(p1, p2, i)
+n0 = n1 = 2**(int(np.log2(len(x0)))+1)
+print("n0 length = %d" %n0)
+
+######################################################
+#  此部分为叠加正弦噪声
+######################################################
+# noise_sin1 = np.sin(2*np.pi*sigma0/16*x0)
+# noise_sin1 = np.sin(2*np.pi*sigma0/8*x0)
+# noise_sin1 = np.sin(2*np.pi*sigma0/4*x0)
+noise_sin1 = np.sin(2*np.pi*sigma0/2*x0)
+
+I0 = np.cos(2*np.pi*sigma0*x0)
+Iw0 = I0 + noise_sin1
+
+Y0, Yw0 = GetFFT(I0, Iw0, n0)
+
+# 设置频谱图的横坐标
+fs_0 = 1/i*np.arange(n0/2)/n0
+
+best_Y0_average_range = 0.5*np.ones((Y0.size, 1))
+
+gs = gridspec.GridSpec(3, 6)
+gs.update(wspace=0.5, hspace=0.5)
+
+
+
+figure(1)
+subplot(gs[0, 0:3])
+plt.plot(x0, I0)
+plt.xlim(-6*(10**(-6)), 6*(10**(-6)))
+plt.title("$I_0$ Original Interferogram")
+
+
+subplot(gs[0, 3:6])
+Y0_pic1, = plt.plot(fs_0, Y0, marker='o', ms=5)
+bf, = plt.plot(fs_0,best_Y0_average_range)
+plt.legend(handles=[Y0_pic1, bf],labels=['Original wave', 'FWHM = 3086 $m^{-1}$'], loc='upper right')
+plt.title("$I_0$ Original spectrum curve")
+plt.xlim(1.82*(10**6), 1.94*(10**6))
+plt.xlabel('Wave number($m^{-1}$)')
+
+subplot(gs[1, 0:3])
+plt.plot(x0, Iw0)
+plt.xlim(-6*(10**(-6)), 6*(10**(-6)))
+plt.title("Interferogram of $I_0$ after superimposing sine noise")
+
+subplot(gs[1, 3:6])
+Yw0_pic1, = plt.plot(fs_0, Yw0, marker='o', ms=5)
+bf, = plt.plot(fs_0,best_Y0_average_range)
+plt.legend(handles=[Yw0_pic1, bf],labels=[r'$\varepsilon = sin(2\pi\frac{\sigma_0}{2}x)$', 'FWHM = 3086 $m^{-1}$'], loc='upper right')
+plt.title("Spectral graph of $I_0$ after superimposing sine noise")
+plt.xlim(1.82*(10**6), 1.94*(10**6))
+plt.xlabel('Wave number($m^{-1}$)')
+
+
+subplot(gs[2, :6])
+
+Yw0_pic1, = plt.plot(fs_0, Yw0, marker='o', ms=5)
+Y0_pic1, = plt.plot(fs_0, Y0, marker='o', ms=5)
+bf, = plt.plot(fs_0,best_Y0_average_range)
+plt.legend(handles=[ Yw0_pic1, Y0_pic1, bf],labels=[r'$\varepsilon = sin(2\pi\frac{\sigma_0}{2}x)$', 'Original wavw', 'FWHM = 3086 $m^{-1}$'], bbox_to_anchor=(0.9,1), loc='best')
+plt.xlim(0*(10**6), 1.99*(10**6))
+plt.xlabel('Wave number($m^{-1}$)')
+plt.title("Spectral graph of $I_0$ before and after superimposing sine noise")
+
+plt.suptitle("532nm - Comparison of the Original Signal and the Signal after Adding Sinusoidal Noise", fontsize = 20)
+
+
+plt.show()
